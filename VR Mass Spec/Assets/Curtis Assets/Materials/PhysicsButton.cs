176c5d65c1e05c6be734b3f953a16bc0f8863c59
silentfox94 <curtis.salley.cs@gmail.com>
2022-04-22 12:45:47 -0700

    Curtis Folder
    This should have all materials, prefabs, and meshes.

---
+++ VR Mass Spec/Assets/Curtis Assets/Materials/PhysicsButton.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PhysicsButton : MonoBehaviour
+{
+    [SerializeField] private float threshold = 0.1f;
+    [SerializeField] private float deadZone = 0.025f;
+
+    public bool isPressed;
+    private Vector3 startPos;
+    private ConfigurableJoint joint;
+
+    public UnityEvent onPressed, onReleased;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.localPosition;
+        joint = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPressed && GetValue() + threshold >=1)
+        {
+            Pressed();
+        }
+
+        if (isPressed && GetValue() - threshold <= 0)
+        {
+            Released();
+        }
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(startPos, transform.localPosition) / joint.linearLimit.limit;
+
+        if (Mathf.Abs(value) < deadZone)
+        {
+            value = 0;
+        }
+
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+    private void Pressed()
+    {
+        isPressed = true;
+        onPressed.Invoke();
+
+    }
+
+    private void Released()
+    {
+        isPressed = false;
+        onReleased.Invoke();
+
+    }
+    public void DebugPress()
+    {
+        Debug.Log("Button is pressed");
+    }
+}
